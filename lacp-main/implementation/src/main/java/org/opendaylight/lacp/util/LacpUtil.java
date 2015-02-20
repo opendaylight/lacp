ff1298c981ef1a483d44ba1d4a5bc1ab0e5063eb
kalaiselvik <Kalaiselvi_K@Dell.com>
2015-02-19 21:54:57 -0800

    Lacp Constant values
    Change-Id: Id375084da61888ab880d021a07657becd031b1be
    Signed-off-by: kalaiselvik <Kalaiselvi_K@Dell.com>

---
+++ lacp-main/implementation/src/main/java/org/opendaylight/lacp/util/LacpUtil.java
@@ -0,0 +1,16 @@
+/* 
+ * Copyright (c) 2014 Dell Inc. and others.  All rights reserved. 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.lacp.util;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.MacAddress;
+
+public class LacpUtil 
+{
+    public static final int LACP_ETHTYPE = 34825;
+    public static final MacAddress LACP_MAC = new MacAddress ("01:80:c2:00:00:02");
+}
