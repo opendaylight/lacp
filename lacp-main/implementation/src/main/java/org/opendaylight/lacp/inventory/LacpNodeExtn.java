28377103d79c5357b45eea2b337d44b8e4237c8a
Kavitha_Ramalingam <Kavitha_Ramalingham@Dell.com>
2015-05-29 12:20:31 +0530

    Revert Group chaining changes as its not currently supported in add-flows
    Change-Id: I30302c63ed44abe591814646fb4a717ee54f6d07
    Signed-off-by: Kavitha_Ramalingam <Kavitha_Ramalingham@Dell.com>

--- lacp-main/implementation/src/main/java/org/opendaylight/lacp/inventory/LacpNodeExtn.java
+++ lacp-main/implementation/src/main/java/org/opendaylight/lacp/inventory/LacpNodeExtn.java
@@ -131,17 +131,17 @@ public class LacpNodeExtn
             if (firstGrpAdd)
             {
                 LOG.debug("creating non-lag group id {} ", groupId);
-                bcastGroup = groupTbl.lacpAddGroup (false, new NodeConnectorRef(port), bcastGroupId, null);
+                bcastGroup = groupTbl.lacpAddGroup (false, new NodeConnectorRef(port), bcastGroupId);
                 firstGrpAdd = false;
                 lacpBuilder.setNonLagGroupid(groupId);
                 LOG.debug("created non-lag group id {} ", groupId);
                 updateLacpNodeDS(nodeInstId);
-            }
-            else
-            {
-                bcastGroup = groupTbl.lacpAddPort(false, new NodeConnectorRef(port), bcastGroup, null);
-            }
-        }
+        	}
+        	else
+        	{
+            		bcastGroup = groupTbl.lacpAddPort(false, new NodeConnectorRef(port), bcastGroup);
+        	}
+	    }
         updateNodeConnectorLacpInfo (port);
         return true;
     }
@@ -170,7 +170,7 @@ public class LacpNodeExtn
         if (result == true)
         {
             bcastGroup = groupTbl.lacpRemPort (bcastGroup, new NodeConnectorRef(port), 
-                                                false, null);
+                                                false);
         }
         return result;
     }
@@ -404,40 +404,4 @@ public class LacpNodeExtn
         return aggId;
     }
 
-    public GroupId getbcastGroupId()
-    {
-	return bcastGroupId;
-    }
-
-    public Group getbcastGroup()
-    {
-	return bcastGroup;
-    }
-
-    public Group setbcastGroup(NodeConnectorRef ncRef, GroupId lagGroupId, boolean isAdd)
-    {
-	synchronized (groupTbl)
-	{
-		if (ncRef == null)
-		{
-			LOG.warn("setbcastGroup: NodeConnector is NULL");
-			return null;
-		}
-		InstanceIdentifier<NodeConnector> ncInstId = (InstanceIdentifier<NodeConnector>)ncRef.getValue();
-
-		InstanceIdentifier<Node> nodeInstId = ncInstId.firstIdentifierOf(Node.class);
-		if (firstGrpAdd)
-		{
-			bcastGroup = groupTbl.lacpAddGroup (false, ncRef, bcastGroupId, lagGroupId);
-			firstGrpAdd = false;
-		}
-		else
-		{
-			bcastGroup = groupTbl.lacpAddRemGroupId(false, nodeInstId, bcastGroup,
-						lagGroupId, isAdd);
-		}
-	}
-
-	return bcastGroup;
-     }
 }
