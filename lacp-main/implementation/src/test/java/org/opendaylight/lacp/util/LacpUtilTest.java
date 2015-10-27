2409e663b1c683897f7e4ccccb8c31090f0f86b1
Aashiha <Aashiha_Priyadarshni@Dell.com>
2015-10-27 09:40:00 +0000

    LacpUtilTest.java
    Change-Id: I8983c82217a2a569cec426c228da12e1b9027c60
    Signed-off-by: Aashiha <Aashiha_Priyadarshni@Dell.com>

---
+++ lacp-main/implementation/src/test/java/org/opendaylight/lacp/util/LacpUtilTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2013, 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.lacp.util;
+
+import static org.junit.Assert.*;
+
+import java.net.UnknownHostException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LacpUtilTest 
+{
+
+	@Before
+	public void initMocks() 
+	{
+		
+	}
+	
+	@Test
+	public void test() 
+	{
+		assertNotNull("Not yet implemented");
+	}
+	
+	@Test
+	public void macToStringTest()
+	{
+		String srcstr = null;
+		assertEquals("null", LacpUtil.macToString(srcstr));
+		srcstr = "AA:11:BB:00:01:00";
+		String srcstr_1 = "AA11BB000100";
+		assertEquals(srcstr_1, LacpUtil.macToString(srcstr));
+	}
+	
+	@Test
+	public void byteToStringTest()
+	{
+		byte[] byteArray = { (byte) 00, (byte)00, (byte) 00, (byte) 01 };
+		String mac_str = "0001";
+		assertEquals(mac_str, LacpUtil.bytetoString(byteArray));
+	}
+	
+	@Test
+	public void convertStringToByteArrayTest()
+	{
+		String mac_str = "0001";
+		assertNotNull(LacpUtil.convertStringtoByte(mac_str));
+	}
+	
+	@Test
+	public void hexStringToByteArrayTest()
+	{
+		byte[] byteArray_1;
+		String hex_String = "09348179d466baa4ab2980ef998ef89efa";
+		String hex_String_1 = "a";
+		byteArray_1 = LacpUtil.hexStringToByteArray(hex_String);
+		assertNotNull(byteArray_1);
+		byteArray_1 = LacpUtil.hexStringToByteArray(hex_String_1);
+		assertNotNull(byteArray_1);
+	}
+	
+}
