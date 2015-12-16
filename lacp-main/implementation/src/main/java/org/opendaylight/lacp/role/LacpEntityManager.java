15c54bc0b2c83d89751ffe4dae54840c23d8bf54
Rajesh_Sindagi <Rajesh_Sindagi@dell.com>
2015-12-16 19:10:46 +0530

    LACP registration with clustering service. Listen and process role notification. Comments incorporated. Basic testing completed.
    Change-Id: I3e47cd704b7453f0e048edb4b724be6e181afa3d
    Signed-off-by: Rajesh_Sindagi <Rajesh_Sindagi@dell.com>

---
+++ lacp-main/implementation/src/main/java/org/opendaylight/lacp/role/LacpEntityManager.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2015 Dell.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.lacp.role;
+
+import com.google.common.base.Optional;
+import org.opendaylight.controller.md.sal.common.api.clustering.CandidateAlreadyRegisteredException;
+import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipState;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipCandidateRegistration;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipChange;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipListenerRegistration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LacpEntityManager {
+	private static final Logger LOG = LoggerFactory.getLogger(LacpEntityManager.class);
+	private EntityOwnershipService entityOwnershipService;
+	private EntityOwnershipCandidateRegistration entityRegistration;
+        private EntityOwnershipListenerRegistration entityOwnershipListenerRegistration;
+	private final LacpOwnershipListener ownershipListener;
+	private final AtomicBoolean isListenerRegistered = new AtomicBoolean();
+
+	public LacpEntityManager( EntityOwnershipService entityOwnershipService ) {
+		this.entityOwnershipService = entityOwnershipService;
+		ownershipListener = new LacpOwnershipListener(this);
+	}
+
+	public void requestLacpEntityOwnership(String appName) {
+
+		final Entity entity = new Entity(appName, appName);
+                try {
+                        LOG.info("requestLacpEntityOwnership: Before Calling registerCandidate", entity);
+                        entityRegistration = entityOwnershipService.registerCandidate(entity);
+                        LOG.info("requestLacpEntityOwnership: After Calling registerCandidate", entity);
+                } catch (CandidateAlreadyRegisteredException e) {
+                        LOG.warn("Candidate - Entity already registered with LACP candidate ", entity, e );
+                }
+
+                try {
+		    if (isListenerRegistered.compareAndSet(false, true)) {
+			entityOwnershipListenerRegistration = entityOwnershipService.registerListener(appName, ownershipListener);
+		    }
+
+		    Optional <EntityOwnershipState> entityOwnershipStateOptional = 
+								entityOwnershipService.getOwnershipState(entity);
+
+		    if (entityOwnershipStateOptional != null && entityOwnershipStateOptional.isPresent()) {
+			final EntityOwnershipState entityOwnershipState = entityOwnershipStateOptional.get();
+			if (entityOwnershipState.hasOwner()) {
+				LOG.info("requestLacpEntityOwnership: An owner exist for entity {} ", 
+						entity);
+				if (entityOwnershipState.isOwner()) {
+					LOG.info("requestLacpEntityOwnership: Becoming Master for entity {} ",
+							entity);
+				} else {
+					LOG.info("requestLacpEntityOwnership: Becoming Slave for entity {} ",
+							entity);
+				}
+                        }
+		    }
+                } catch (Exception e){
+                    LOG.warn("Exception while registering listener with entityOwnershipService ", appName , e );
+                }
+        }
+
+	public void onRoleChanged(EntityOwnershipChange ownershipChange) {
+            final Entity entity = ownershipChange.getEntity();
+            if (ownershipChange.isOwner())
+            {
+                LOG.info("onRoleChanged: BECAME MASTER - {} " , entity);
+            }
+            else
+            {
+                LOG.info("onRoleChanged: BECAME SLAVE - {} ", entity);
+            }
+        }
+
+        public void closeListeners()
+        {
+            LOG.info("closeListeners : calling entityRegistration close method");
+            if(entityRegistration != null) {
+                entityRegistration.close();
+            }
+            if(entityOwnershipListenerRegistration != null) {
+                entityOwnershipListenerRegistration.close();
+            }
+        }
+}
